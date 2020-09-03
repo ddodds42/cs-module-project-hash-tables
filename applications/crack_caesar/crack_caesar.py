f56608b5d2b3ee2206027ee2b4fafc052ab9f604
ddodds42 <57008540+ddodds42@users.noreply.github.com>
2020-09-03 17:12:04 -0400

    Caeser committed genocide

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -1,5 +1,23 @@
 # Use frequency analysis to find the key to ciphertext.txt, and then
 # decode it.
 
-# Your code here
+qraqt = {
+    'A':'C','B':'F','C':'H','D':'N','E':'M','F':'Y','G':'U','H':'V',
+    'I':'I','J':'T','K':'R','L':'X','M':'A','N':'S','O':'W','P':'K',
+    'Q':'G','R':'Z','S':'L','T':'J','U':'D','V':'Q','W':'E','X':'O',
+    'Y':'B','Z':'P'
+}
 
+with open("ciphertext.txt") as f:
+    corpus = f.read()
+
+def brutus(s):
+    decoded = ""
+    for let in s:
+        if let in qraqt:
+            decoded += qraqt[let]
+        else:
+            decoded += let
+    return decoded
+
+print(brutus(corpus))
